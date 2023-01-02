80329464d82ab36369d8b48cb2dcff21d09813d3
noahshotz <max.heinrich@uusimaa.de>
2023-01-03 00:52:52 +0100

    optimized for warnings

--- src/main/java/com/mst/App.java
+++ src/main/java/com/mst/App.java
@@ -8,26 +8,8 @@ import graph.*;
 public final class App {
     public static void main(String[] args) {
 
-        Graph myGraph = new Graph<>(false);
-        Vertex a = new Vertex(0, "A");
-        Vertex b = new Vertex(1, "B");
-        Vertex c = new Vertex(2, "C");
-
-        myGraph.addVertex(a);
-        myGraph.addVertex(b);
-        myGraph.addVertex(c);
-
-        Edge ab = new Edge(a, b, 10);
-        Edge ac = new Edge(a, c, 20);
-        Edge bc = new Edge(b, c, 30);
-
-        myGraph.addEdge(ab);
-        myGraph.addEdge(ac);
-        myGraph.addEdge(bc);
-
-        System.out.println("------------");
-        System.out.println("Current Graph: ");
-        System.out.println(myGraph);
+        primsalgorithm newObj = new primsalgorithm();
+        newObj.solve();
 
     }
 }
--- src/main/java/com/mst/primsalgorithm.java
+++ src/main/java/com/mst/primsalgorithm.java
@@ -4,11 +4,67 @@ import graph.*;
 
 public class primsalgorithm {
 
-    public static void primsSolver() {
+    public void solve() {
 
-        // eingabe-graph nehmen
+        // graphen nehmen
+        Graph<Vertex, Edge<Vertex>> myGraph = new Graph<>(false);
+        Vertex a = new Vertex(3, "A");
+        Vertex b = new Vertex(1, "B");
+        Vertex c = new Vertex(2, "C");
 
-        // startknoten: knoten mit der kleinsten id
+        myGraph.addVertex(a);
+        myGraph.addVertex(b);
+        myGraph.addVertex(c);
+
+        Edge<Vertex> ab = new Edge<>(a, b, 10);
+        Edge<Vertex> ac = new Edge<>(a, c, 20);
+        Edge<Vertex> bc = new Edge<>(b, c, 30);
+
+        myGraph.addEdge(ab);
+        myGraph.addEdge(ac);
+        myGraph.addEdge(bc);
+
+        System.out.println("----------");
+        System.out.println("Current graph: ");
+        System.out.println(myGraph);
+
+        // startknoten finden
+        System.out.println("----------");
+        System.out.println("Alle Knoten: ");
+        System.out.println(myGraph.getVertices());
+
+        System.out.println("----------");
+        System.out.println("Startknoten: ");
+
+        // startknoten: knoten mit der kleinsten id finden
+        boolean startFound = false;
+        int i = 0;
+        int startingPoint = 0;
+        while (!startFound) {
+            Vertex start = myGraph.getVertex(i);
+            if (start != null) {
+                startingPoint = i;
+                startFound = true;
+            }
+            i++;
+        }
+        System.out.println(startingPoint);
+
+        // für Startknoten alle Nachbarn finden
+        System.out.println("----------");
+        System.out.println("Alle Nachbarn:");
+        System.out.println(myGraph.getNeighbours(startingPoint));
+
+        // für alle Edges zu den Nachbarn die Edge mit den
+        // niedrigsten Kosten finden
+        System.out.println("----------");
+        System.out.println("VertexA:");
+        System.out.println(ab.getVertexA());
+        System.out.println("VertexB:");
+        System.out.println(ab.getVertexB());
+
+
+
 
     }
 
